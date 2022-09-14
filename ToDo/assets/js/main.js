5244ed709d7f5835ba1046b11d1ee5812aa02414
Farid <faridyn@code.edu.az>
2022-09-14 11:42:13 +0400

    ToDo

---
+++ ToDo/assets/js/main.js
@@ -0,0 +1,5 @@
+btn_add.onclick = function() {
+    var div = document.createElement('div');
+    div.setAttribute('class','box')
+    icon.setAttribute('class','fa-solid')
+    var todo = document.getElementById('todo');
